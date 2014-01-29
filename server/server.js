d94f75da0ee151f98457b03bf0f6177059a40ac6
Doug Horner <horner@mieweb.com>
2014-01-29 01:29:24 -0500

    Added Haraka as the mail server.  It runs on port 2525 for now.

--- server/server.js
+++ server/server.js
@@ -1,4 +1,21 @@
 
+
+// Start up server processes.  
+
+// SMTP
+// The dir /private becomes assets/app when meteor builds the site.  This tells Haraka to use that directory as it's base dir.
+process.env.HARAKA=process.cwd()+'/assets/app';
+console.log("ENV:",process.env.HARAKA);
+
+var Haraka = Meteor.require('Haraka');
+
+// DNS
+
+// LDAP
+
+// IMAP (future)
+
+
 Meteor.publish("directory", function () {
   return Meteor.users.find({}, {fields: {emails: 1, profile: 1}});
 });
@@ -7,3 +24,6 @@ Meteor.publish("Messages", function () {
   return Messages.find(
     {$or: [{"public": true}, {invited: this.userId}, {owner: this.userId}]});
 });
+
+
+
